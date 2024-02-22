bf8541be1f19b4bc577e9bea751b853b30bea91b
jeyashriJ <jeyashri30@gmail.com>
2024-02-22 23:03:04 +0530

    initial version

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="header">
       <LightbulbIcon className="header-icon" />
-      <h1 className="header-h1">todoapp</h1>
+      <h1 className="header-h1">myTodoapp</h1>
     </div>
   );
 }
