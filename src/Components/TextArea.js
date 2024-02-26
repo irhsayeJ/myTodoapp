6afa6a3270614d451eac9df7ace28dba5dbddbc6
jeyashriJ <jeyashri30@gmail.com>
2024-02-27 00:19:11 +0530

    added title expand

--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 function TextArea(props) {
   const [note, setNote] = useState({ title: "", content: "" });
+  const [isExpanded, setExpanded] = useState(false);
   function handleChange(event) {
     const { name, value } = event.target;
     setNote((prevvalue) => {
@@ -16,23 +17,29 @@ function TextArea(props) {
     event.preventDefault();
     setNote({ title: "", content: "" });
   }
+  function expand() {
+    setExpanded(true);
+  }
   return (
     <div className="textArea">
       <form className="textArea-form">
-        <input
-          name="title"
-          className="form-input"
-          placeholder="Enter your title"
-          onChange={handleChange}
-          value={note.title}
-        ></input>
+        {isExpanded && (
+          <input
+            name="title"
+            className="form-input"
+            placeholder="Enter your title"
+            onChange={handleChange}
+            value={note.title}
+          ></input>
+        )}
         <textarea
           name="content"
           className="form-textArea"
           placeholder="start typing..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
           onChange={handleChange}
           value={note.content}
+          onClick={expand}
         ></textarea>
         <button onClick={handleSubmit} className="form-button">
           <AddCircleIcon />
